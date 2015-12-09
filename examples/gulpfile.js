cc3130f29de4ff634dbe00ee9258989e059d52f6
Taras Serdyuk <taras@techburg.io>
2015-12-09 19:49:01 +0300

    examples list

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -1,1 +1,26 @@
 import gulp from 'gulp'
+import _ from 'gulp-load-plugins'
+
+gulp.task('jade', () => {
+
+})
+
+gulp.task('jade-plumber', () => {
+
+})
+
+gulp.task('jade-notify', () => {
+
+})
+
+gulp.task('sass', () => {
+
+})
+
+gulp.task('sass-plumber', () => {
+
+})
+
+gulp.task('sass-notify', () => {
+
+})
