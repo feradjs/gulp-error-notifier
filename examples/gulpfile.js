8f273aacabb1dc7318a2dccfc6b8e5c5a3a22ad4
Taras Serdyuk <taras@techburg.io>
2015-12-09 19:58:33 +0300

    jade examples

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -1,16 +1,24 @@
 import gulp from 'gulp'
 import _ from 'gulp-load-plugins'
+import errorNotifier from 'gulp-error-notifier'
 
 gulp.task('jade', () => {
-
+	gulp.src('src/*.jade')
+		.pipe(_.jade())
+		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('jade-plumber', () => {
-
+	gulp.src('src/*.jade')
+		.pipe(_.plumber())
+		.pipe(_.jade())
+		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('jade-notify', () => {
-
+	gulp.src('src/*.jade')
+		.pipe(errorNotifier())
+		.pipe(gulp.dest('dist'))
 })
 
 gulp.task('sass', () => {
